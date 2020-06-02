a2ca3354f70a89dd2a4d48d427fefa1544cb5ca8
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 19:17:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,10 @@
 def my_each(array) # put argument(s) here
  counter = 0 
- while counter <array.length 
+ while counter < array.length # [blue, red, green, yellow]
+ yield array[counter]
+ counter += 1 
+ end 
   # code here
-enddef
+
+ array
+end 
